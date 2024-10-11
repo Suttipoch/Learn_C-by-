eb807e92f6226ffb9ad0daf68c553556c24cb7dd
ReVerse303 <tomatobest2547@gmail.com>
2024-10-11 21:48:56 +0700

    add_Test-Prime-2

--- Test-Prime-2/main.c
+++ Test-Prime-2/main.c
@@ -2,6 +2,38 @@
 #include <stdlib.h>
 
 int main(){
+    int number, isPrime = 0;
+    printf("Please enter an integer <between 2 to 9999999>: ");
+    scanf("%d",&number);
+
+    if(number<2 || number>9999999){
+        printf("%d ",number);
+        printf("is not between 2 to 9999999,please re-enter.\n");
+    }else{
+        if(number==2){
+            isPrime=0;
+
+        }
+
+        for(int i=2;i<number;i++){
+
+            if(number%i==0){
+                isPrime = 1;
+                break;
+
+            }else{
+                isPrime = 0;
+            }
+        }
+    }
+    if(isPrime==0){
+        printf("%d ",number);
+        printf("is a prime number.");
+    }else{
+        printf("%d ",number);
+        printf("is a not prime number.");
+    }
+
 
 
 
